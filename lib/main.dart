ffb75936c7c43df2f1e367be0298fd9031eeffda
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-05-04 00:39:58 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -26,3 +26,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/*
+Future<void> loadImageMatrix() async {
+  final matrix =
+      await Temp.convertAssetImageTo1DArray('assets/images/dog.11.jpg');
+  final matrix2 =
+      await Temp.convertAssetImageTo1DArray('assets/images/cat.10.jpg');
+
+  final matrix3 =
+      await Temp.convertAssetImageTo1DArray('assets/images/dog.10.jpg');
+
+  final matrix4 =
+      await Utilities.normalizedPixelValues('assets/images/dog.11.jpg');
+  final matrix5 =
+      await Utilities.normalizedPixelValues('assets/images/cat.10.jpg');
+  final matrix6 =
+      await Utilities.normalizedPixelValues('assets/images/dog.10.jpg');
+  List<List<double>> l1 = [matrix4, matrix5];
+  final bias = [0, 0];
+  final h2 = HammingNeuralNetwork(l1, bias);
+  final result1 = h2.run(matrix6);
+  print('First Result: $result1');
+
+  Hamming h1 = Hamming(weights: l1, input: matrix6);
+  print('Second Result : ${h1.result()}');
+
+  print(' this length matrix1 : ${matrix.length}');
+  print(' this length matrix2 : ${matrix2.length}');
+  print(' this length matrix3 : ${matrix3.length}');
+  print(' this length matrix4 : ${matrix4.length}');
+
+  print(' this matrix1 : $matrix');
+  print(' this matrix2 : $matrix2');
+  print('matrix 3 : $matrix3');
+  print('matrix 4 : $matrix4');
+
+  List<List<double>> l = [matrix, matrix2];
+  Hamming h = Hamming(weights: l, input: matrix3);
+  print('Thired Result : ${h.result()}');
+
+  final perceptron = Perceptron(numInputs: 400, learningRate: 0.1);
+  perceptron.train([matrix, matrix2], [1, -1]);
+
+  final prediction = perceptron.predict(matrix3);
+  print('Prediction: $prediction');
+}
+*/
+
