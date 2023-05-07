4d0056055956521df22d9cfebde5511d8a86cf37
AhmedRamy2000 <125711699+AhmedRamy2000@users.noreply.github.com>
2023-05-08 02:33:42 +0300

    ui updata

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -17,80 +17,65 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         title: const Text('Ai Recognition'),
       ),
-      body: DecoratedBox(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/back.jfif"), fit: BoxFit.cover),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: SizedBox(
-                        height: 50,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.orange,
-                          ),
-                          onPressed: () =>
-                              Navigator.pushNamed(context, 'hamming'),
-                          child: const Text('Hamming',
-                              style: TextStyle(
-                                fontSize: 20,
-                              )),
-                        ),
+      body: Container(
+        width: double.infinity,
+        child: DecoratedBox(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/back.jfif"), fit: BoxFit.cover),
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Spacer(flex: 3),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                    ),
+                    onPressed: () => Navigator.pushNamed(context, 'hamming'),
+                    child: const Text(' Hamming ',
+                        style: TextStyle(
+                          fontSize: 20,
+                        )),
+                  ),
+                ),
+                const Spacer(flex: 1),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepOrange),
+                    onPressed: () => Navigator.pushNamed(context, 'perceptron'),
+                    child: const Text(
+                      'Perceptron',
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: SizedBox(
-                        height: 50,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.deepOrange),
-                          onPressed: () =>
-                              Navigator.pushNamed(context, 'perceptron'),
-                          child: const Text(
-                            'Perceptron',
-                            style: TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ),
+                ),
+                const Spacer(flex: 1),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
                     ),
+                    onPressed: () => Navigator.pushNamed(context, 'Hebbian'),
+                    child: const Text('  Hebbian  ',
+                        style: TextStyle(
+                          fontSize: 20,
+                        )),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: SizedBox(
-                        height: 50,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.orange,
-                          ),
-                          onPressed: () =>
-                              Navigator.pushNamed(context, 'Hebbian'),
-                          child: const Text('Hebbian',
-                              style: TextStyle(
-                                fontSize: 20,
-                              )),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
+                ),
+                const Spacer(flex: 3),
+              ],
+            ),
           ),
         ),
       ),
